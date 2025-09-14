c1e9754387286e5c06649c5e1eb133bcc007d6fd
Master Barreto <jpanela302@gmail.com>
2025-09-14 16:12:39 +0000

    a tela inicial era para estar assim

--- src/app/teacher/dashboard/page.tsx
+++ src/app/teacher/dashboard/page.tsx
@@ -5,19 +5,21 @@ export default function DashboardPage() {
   const courses = ['1º Ano', '2º Ano', '3º Ano'];
 
   return (
-    <div className="flex flex-col items-center w-full">
-      <h1 className="text-4xl font-bold mb-12">Bem-vindos Professores</h1>
+    <div className="flex flex-col">
+      <h1 className="text-4xl font-bold mb-12 text-center">
+        Bem-vindos Professores
+      </h1>
       <div className="w-full max-w-4xl">
         <h2 className="text-2xl font-bold mb-6 text-left">Seus Cursos</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {courses.map((course) => (
             <Card
               key={course}
               className="bg-[#111115] border-gray-800 rounded-lg p-4 flex flex-col items-center justify-center aspect-square text-white hover:bg-gray-800 transition-colors"
             >
               <CardContent className="flex flex-col items-center justify-center p-0">
-                <GraduationCap className="h-16 w-16 mb-4" />
-                <p className="font-semibold text-lg">{course}</p>
+                <GraduationCap className="h-12 w-12 mb-4" />
+                <p className="font-semibold text-md">{course}</p>
               </CardContent>
             </Card>
           ))}
